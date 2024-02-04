0c0ae7f4000f448985804b7b5f64c5b0eb96f8af
Antar Baishnab <107618683+antar2001@users.noreply.github.com>
2024-02-04 05:04:39 -0600

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,21 @@
+print("Hello world")
+message="Hello Python world"
+print(message)
+name= "antar baishnab"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name="antar"
+last_name="baishnab"
+full_name=first_name+" "+last_name
+print(full_name)
+print(full_name.title())
+print("Hello, "+full_name.title())
+print("python")
+print("\tpython")
+print("Antar\n\tBaishnab")
+age="23"
+message=first_name+" "+age
+print(message)
+
+
